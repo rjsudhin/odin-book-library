69ab031a8d529e04d934fe3887d3ee9d43eca7e4
SudhinRj <rjsudhin@users.noreply.github.com>
2025-12-06 13:01:18 +0530

    form ui fixed not yet complets the getting result of users

--- js/book.js
+++ js/book.js
@@ -1,6 +1,23 @@
-const button = document.querySelector('button')
+const header = document.querySelector('header')
+const button = document.querySelector('.create-book-btn')
+const main = document.querySelector('main')
+const form = document.querySelector('form')
+const formSubmitBtn = document.querySelector('.submit-btn')
+
+
+// desable while form first load
+form.style.display = 'none'
+
+// add button clicks
 button.addEventListener('click', addBookToLibrary)
 
+
+// new button submision clicks
+formSubmitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log('submission clicks')
+  gettingUserInputValues()
+})
 
 const myLibrary = []
 
@@ -17,15 +34,25 @@ function Book(title, author, pages, read, id) {
 
 
 function addBookToLibrary() {
-  let randomId = crypto.randomUUID()
-  const book1 = new Book('Dragon Ball', 'makia', 882, true, randomId)
-  myLibrary.push(book1)
-  showCaseLibrary()
+  addingInputUi()
 }
+
+// getting user input values
+function gettingUserInputValues() {
+  
+}
+
 
 
 function showCaseLibrary() {
   if (myLibrary.length >= 0) {
     console.log(myLibrary)
   }
+}
+
+
+function addingInputUi() {
+  form.style.display = 'flex'
+  // remove the adding button then
+  header.removeChild(button)
 }
