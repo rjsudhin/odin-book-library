03089f87abff322ff7dbb650003b6df3ef8e95b7
SudhinRj <rjsudhin@users.noreply.github.com>
2025-12-07 11:03:11 +0530

    creating book cards with Book constructon and added library Array

--- js/book.js
+++ js/book.js
@@ -4,6 +4,9 @@ const main = document.querySelector('main')
 const form = document.querySelector('form')
 const formSubmitBtn = document.querySelector('.submit-btn')
 
+let titleInput = document.querySelector('#title')
+let authorInput = document.querySelector('#author')
+let pagesInput = document.querySelector('#pages')
 
 // desable while form first load
 form.style.display = 'none'
@@ -39,7 +42,28 @@ function addBookToLibrary() {
 
 // getting user input values
 function gettingUserInputValues() {
-  
+  let title = titleInput.value 
+  let author = authorInput.value 
+  let pages = pagesInput.value 
+  let radioSelection = ''
+  let selectedRadioButton = document.querySelector('input[name="read-report"]:checked')
+  if (selectedRadioButton) {
+    console.log(`selected Radio button value : ${selectedRadioButton.value}`)
+    if (selectedRadioButton.value == 'yes') {
+      radioSelection = true
+    } else {
+      radioSelection = false
+    }
+  } else {
+    console.log('No radio button in the gorup is selected')
+  }
+  console.log(radioSelection)
+
+  // get a random crypto id
+  let randomCryptoId = crypto.randomUUID()
+  // make a new book card 
+  let obj = new Book(title, author, pages, radioSelection, randomCryptoId)
+  myLibrary.push(obj)
 }
 
 
