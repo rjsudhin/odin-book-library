9c53a2875bb999d52d6790c90bb01c2c7b505dcb
SudhinRj <rjsudhin@users.noreply.github.com>
2025-12-08 14:01:42 +0530

    added localstorage to entered object books

--- js/book.js
+++ js/book.js
@@ -22,9 +22,9 @@ formSubmitBtn.addEventListener('click', (e) => {
   gettingUserInputValues()
 })
 
-const myLibrary = []
+let myLibrary = []
 
-showCaseLibrary()
+addingLocalStorage()
 
 // book constructor
 function Book(title, author, pages, read, id) {
@@ -63,20 +63,67 @@ function gettingUserInputValues() {
   let randomCryptoId = crypto.randomUUID()
   // make a new book card 
   let obj = new Book(title, author, pages, radioSelection, randomCryptoId)
+  // adding created object in mylibrary arr[]
+
+  addingLocalStorage()
+  retrievingExistingData(obj)
+  removeFormUI()
+  toggleAddButton()
+  resetInputValues()
+}
+
+function addingLocalStorage() {
+  let jsonString = JSON.stringify(myLibrary)
+
+  // store the JSON string in localStorage with a name
+  localStorage.setItem('collection', jsonString)
+}
+
+function retrievingExistingData(obj) {
+  let sortedCollection = localStorage.getItem('collection')
+
+  myLibrary = sortedCollection ? JSON.parse(sortedCollection) : []
+
   myLibrary.push(obj)
+
+  // stringify the updated array
+  let updatedCollectionString = JSON.stringify(myLibrary)
+
+  // store the updatd data
+  localStorage.setItem('collection', updatedCollectionString)
+
 }
 
 
+// getting the form ui for getting user inputs
+function addingInputUi() {
+  form.style.display = 'flex'
+  // remove the adding button then
+  toggleAddButton()
+}
 
-function showCaseLibrary() {
-  if (myLibrary.length >= 0) {
-    console.log(myLibrary)
+
+// removing form ui from the main section
+function removeFormUI() {
+  if (form.style.display != 'none') {
+    form.style.display = 'none'
+  }
+}
+
+// removing and adding add button
+function toggleAddButton() {
+  if (header.contains(button)) {
+    header.removeChild(button)
+  } else {
+    header.appendChild(button)
   }
 }
 
 
-function addingInputUi() {
-  form.style.display = 'flex'
-  // remove the adding button then
-  header.removeChild(button)
+// reset \ make input elements to empty
+function resetInputValues() {
+  const inputArr = [titleInput, authorInput, pagesInput]
+  inputArr.forEach(input => {
+    input.value = ''
+  })
 }
